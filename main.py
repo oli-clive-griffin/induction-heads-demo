d713a8902651f6ecadd05891ba2aa767fccb02fe
Changlin Li <mail@changlinli.com>
2024-05-16 16:24:12 -0700

    Commit with wandb scaffolding

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import einops
 import random
+import wandb
+import PIL.Image
 
-device = "mps"
+#%%
+
+device = "cpu"
 
 cache = {}
 
@@ -20,10 +24,11 @@ class AttentionLayer(nn.Module):
         super().__init__()
         self.index = index
         self.d_head = d_head
-        self.Q1 = nn.Linear(d_model, d_head)
-        self.K1 = nn.Linear(d_model, d_head)
-        self.V1 = nn.Linear(d_model, d_head)
-        self.O1 = nn.Linear(d_head, d_model)
+        self.W_Q = nn.Linear(d_model, d_head)
+        self.W_K = nn.Linear(d_model, d_head)
+        self.W_V = nn.Linear(d_model, d_head)
+        self.W_O = nn.Linear(d_head, d_model)
+        self.attn_pattern = None
     
     def forward(self, x: Tensor, with_cache):
         """
@@ -36,20 +41,24 @@ class AttentionLayer(nn.Module):
         #     is_causal=True,
         # )
 
-        Q = self.Q1.forward(x) # (batch_size, seq_len, d_head)
-        K = self.K1.forward(x) # (batch_size, seq_len, d_head)
-        V = self.V1.forward(x) # (batch_size, seq_len, d_head)
+        Q = self.W_Q.forward(x) # (batch_size, seq_len, d_head)
+        K = self.W_K.forward(x) # (batch_size, seq_len, d_head)
+        V = self.W_V.forward(x) # (batch_size, seq_len, d_head)
 
         QK_ = (Q @ K.transpose(-2, -1))
         QK = QK_ / (QK_.shape[-1] ** 0.5)
-        attn = F.softmax(QK, dim=-1)
+        large_negative_number = -1e9
+        mask = torch.ones_like(QK) * large_negative_number
+        mask = torch.triu(mask, diagonal=1)
+        attn = F.softmax(QK + mask, dim=-1)
+        self.attn_pattern = attn
         if with_cache:
             cache[f'Q_{self.index}'] = Q
             cache[f'K_{self.index}'] = K
             cache[f'V_{self.index}'] = V
             cache[f'attn_{self.index}'] = attn
         x = attn @ V
-        return self.O1(x)
+        return self.W_O(x)
 
 
 class Model(nn.Module):
@@ -114,6 +123,7 @@ class Trainer:
         self.batch_size = batch_size
         self.seq_len_limit = seq_len_limit
         self.model = model
+        self.step = 0
 
     def train(self, epochs):
         optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-3)
@@ -124,6 +134,13 @@ class Trainer:
             loss.backward()
             print(f"Epoch {e}, loss: {loss.item()}")
             optimizer.step()
+            # wandb.log({"loss": loss}, self.step)
+            attn_pattern_1 = self.model.attn1.attn_pattern[0].detach().numpy()
+            attn_pattern_1_as_img = PIL.Image.fromarray(wandb.Image.to_uint8(attn_pattern_1))
+            attn_pattern_2 = self.model.attn2.attn_pattern[0].detach().numpy()
+            attn_pattern_2_as_img = PIL.Image.fromarray(wandb.Image.to_uint8(attn_pattern_2))
+            wandb.log({"attn_pattern_1": wandb.Image(attn_pattern_1_as_img), "attn_pattern_2": wandb.Image(attn_pattern_2_as_img)})
+            self.step += 1
 
     def create_example(self):
         x = []
@@ -147,9 +164,13 @@ d_vocab = 1000
 d_model = 512
 d_head = 64
 model = Model(d_model=d_model, d_vocab=d_vocab, d_head=d_head).to(device)
+wandb.init(project="trying-to-find-induction-heads")
 trainer = Trainer(batch_size=256, seq_len_limit=128, model=model)
 trainer.train(10_000)
+wandb.finish()
 
+
+# %%
 
 # %%
 out = model.forward(
@@ -169,14 +190,14 @@ imshow(cache['attn_2'].detach().cpu()[0].numpy())
 from matplotlib.pyplot import imshow
 torch.set_printoptions(precision=4, sci_mode=False)
 
-QK = model.attn1.Q1.weight.T @ model.attn1.K1.weight
+QK = model.attn1.W_Q.weight.T @ model.attn1.W_K.weight
 print(QK)
 imshow(QK.detach().cpu().numpy())
 # imshow(model.attn1.V1.weight.T.detach().cpu().numpy())
 # %%
 
-OV_l1 = model.attn1.O1.weight @ model.attn1.V1.weight
-OV_l2 = model.attn2.O1.weight @ model.attn2.V1.weight
+OV_l1 = model.attn1.W_O.weight @ model.attn1.W_V.weight
+OV_l2 = model.attn2.W_O.weight @ model.attn2.W_V.weight
 
 OV = OV_l2 @ OV_l1
 # print(OV.shape)
@@ -184,7 +205,7 @@ OV = OV_l2 @ OV_l1
 # imshow(OV.detach().cpu().numpy())
 OV.std(), OV.mean(), OV.median(), OV.min(), OV.max()
 # %%
-QK = model.attn2.Q1.weight.T @ model.attn2.K1.weight
+QK = model.attn2.W_Q.weight.T @ model.attn2.W_K.weight
 imshow(QK.detach().cpu().numpy())
 # %%
 
@@ -207,12 +228,12 @@ probs
 
 #%%
 
-model.attn1.O1.weight = saved1
-model.attn2.O1.weight = saved2
+model.attn1.W_O.weight = saved1
+model.attn2.W_O.weight = saved2
 #%%
 
-model.attn1.O1.weight = nn.Parameter(torch.zeros_like(model.attn1.O1.weight))
-model.attn2.O1.weight = nn.Parameter(torch.zeros_like(model.attn2.O1.weight))
+model.attn1.W_O.weight = nn.Parameter(torch.zeros_like(model.attn1.W_O.weight))
+model.attn2.W_O.weight = nn.Parameter(torch.zeros_like(model.attn2.W_O.weight))
 
-model.attn1.O1.weight = nn.Parameter(torch.zeros_like(model.attn1.O1.weight))
-model.attn2.O1.weight = nn.Parameter(torch.zeros_like(model.attn2.O1.weight))
+model.attn1.W_O.weight = nn.Parameter(torch.zeros_like(model.attn1.W_O.weight))
+model.attn2.W_O.weight = nn.Parameter(torch.zeros_like(model.attn2.W_O.weight))
